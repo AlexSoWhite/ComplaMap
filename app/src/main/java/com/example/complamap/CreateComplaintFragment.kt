248483185353015d4ec9c69e59b42d33ff0e0495
katekaa <katrinne253@gmail.com>
2021-10-18 16:57:35 +0300

    nav

---
+++ app/src/main/java/com/example/complamap/CreateComplaintFragment.kt
@@ -0,0 +1,21 @@
+package com.example.complamap
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.complamap.databinding.FragmentMockBinding
+
+class CreateComplaintFragment: Fragment() {
+    private lateinit var binding: FragmentMockBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentMockBinding.inflate(inflater)
+        return  binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.textMock.text = "create complaint"
+    }
+}
