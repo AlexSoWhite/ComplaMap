5e1e01863f6cac939d37298eb711afd52119446e
Ktoxsyrov <ktoxsyrovv@inbox.ru>
2021-10-20 01:55:32 +0300

    Create Complaint

--- app/src/main/java/com/example/complamap/CreateComplaintActivity.kt
+++ app/src/main/java/com/example/complamap/CreateComplaintActivity.kt
@@ -2,6 +2,14 @@ package com.example.complamap
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.complamap.databinding.CreateComplaintActivityBinding
+import android.content.Context
+import android.transition.TransitionManager
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+
 
 class CreateComplaintActivity : AppCompatActivity() {
     private lateinit var binding: CreateComplaintActivityBinding
@@ -11,10 +19,33 @@ class CreateComplaintActivity : AppCompatActivity() {
         binding = CreateComplaintActivityBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        setContentView(R.layout.create_complaint_activity)
+        val addButton: Button = findViewById(R.id.AddButton)//кнопка внизу
+        addButton.setOnClickListener {//обработка нажатия на кнопку внизу
+            showPopup()
+        }
+
     }
 
-    override fun onStart() {
-        super.onStart()
-        binding.textMock.text = "create complaint"
+
+    private fun showPopup() {
+        val rootLayout = findViewById<ViewGroup>(R.id.root_layout)
+        val inflater: LayoutInflater =
+            getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.create_complaint_popup_menu, null)
+        val popupWindow = PopupWindow(
+            view,
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        TransitionManager.beginDelayedTransition(rootLayout)
+        popupWindow.showAtLocation(rootLayout, Gravity.CENTER, 0, 0)
+
+        val closeButton = view.findViewById<Button>(R.id.closePopup) //нажатие на крестик
+        closeButton.setOnClickListener {
+            popupWindow.dismiss()
+        }
     }
 }
+
