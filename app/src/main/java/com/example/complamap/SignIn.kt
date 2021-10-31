66ed38566a807b277d70b551b18d57ca82a79fc2
AlexSoWhite <shura_belov_00@bk.ru>
2021-10-31 19:11:36 +0300

    stashing

---
+++ app/src/main/java/com/example/complamap/SignIn.kt
@@ -0,0 +1,12 @@
+package com.example.complamap
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignIn:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_in_activity)
+    }
+}
