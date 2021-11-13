3a9d69391dfb1253c41118633dca07cba598b2e7
tewat <hollywoodstreets23@gmail.com>
2021-11-13 20:09:40 +0300

    FAB and BottomSheet completely moved to MapFragment

--- app/src/main/java/com/example/complamap/views/fragments/MapFragment.kt
+++ app/src/main/java/com/example/complamap/views/fragments/MapFragment.kt
@@ -1,7 +1,9 @@
 package com.example.complamap.views.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +11,8 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.complamap.R
 import com.example.complamap.databinding.FragmentMapBinding
-import com.example.complamap.databinding.ItemBottomSheetContainerBinding
+import com.example.complamap.views.activities.CreateComplaintActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
@@ -28,16 +31,12 @@ class MapFragment() : Fragment(), GeoObjectTapListener, InputListener {
     private lateinit var mapView: MapView
     private lateinit var placemark: PlacemarkMapObject
     private var isGeoObjectSelected: Boolean = false
-    private var _itemBottomSheetContainerBinding: ItemBottomSheetContainerBinding? = null
-    private val itemBottomSheetContainerBinding get() = _itemBottomSheetContainerBinding!!
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentMapBinding.inflate(inflater, container, false)
-        _itemBottomSheetContainerBinding = ItemBottomSheetContainerBinding.inflate(layoutInflater)
         val view = binding.root
         mapView = binding.mapview
         mapView.map.move(
@@ -45,6 +44,11 @@ class MapFragment() : Fragment(), GeoObjectTapListener, InputListener {
             Animation(Animation.Type.SMOOTH, 0F),
             null
         )
+        setBottomSheetPeekHeight()
+        binding.fab.setOnClickListener {
+            val intent = Intent(requireContext(), CreateComplaintActivity::class.java)
+            startActivity(intent)
+        }
         mapView.map.logo.apply {
             this.setAlignment(Alignment(HorizontalAlignment.RIGHT, VerticalAlignment.TOP))
         }
@@ -104,8 +108,20 @@ class MapFragment() : Fragment(), GeoObjectTapListener, InputListener {
     override fun onMapLongTap(p0: Map, p1: Point) {}
 
     private fun setCoordinates(p1: Point) {
-        itemBottomSheetContainerBinding.coordinatesView.text = "Координаты:  ${p1.longitude}, ${p1.latitude}"
         val coor = activity?.findViewById<TextView>(R.id.coordinates_view)
         coor?.text = "Координаты:  ${p1.longitude}, ${p1.latitude}"
+    }
+
+    private fun setBottomSheetPeekHeight() {
+        val bottomSheetParent = binding.bottomSheetParent.root
+        val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetParent)
+        val tv = TypedValue()
+        if (requireActivity().theme.resolveAttribute(R.attr.actionBarSize, tv, true)) {
+            val actionBarHeight = TypedValue.complexToDimensionPixelSize(
+                tv.data,
+                resources.displayMetrics
+            )
+            bottomSheetBehavior.peekHeight = actionBarHeight * 2
+        }
     }
 }
