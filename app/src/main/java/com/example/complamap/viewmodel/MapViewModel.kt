5f50d25424474da909d63726c2f674ffa5fa02c6
tewat <hollywoodstreets23@gmail.com>
2021-11-15 23:55:20 +0300

    Changes due Ktlint

--- app/src/main/java/com/example/complamap/viewmodel/MapViewModel.kt
+++ app/src/main/java/com/example/complamap/viewmodel/MapViewModel.kt
@@ -1,9 +1,10 @@
 package com.example.complamap.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.complamap.model.OnAddressFetchedListener
+import com.example.complamap.model.OnGeoObjectFetchedListener
 import com.example.complamap.model.PointAddressConverter
 import com.yandex.mapkit.GeoObject
 import com.yandex.mapkit.geometry.Point
@@ -11,7 +12,6 @@ import com.yandex.mapkit.layers.GeoObjectTapEvent
 import com.yandex.mapkit.map.GeoObjectSelectionMetadata
 import com.yandex.mapkit.search.*
 import com.yandex.mapkit.search.search_layer.SearchResultItem
-import com.yandex.runtime.Error
 
 class MapViewModel : ViewModel() {
     private val addressMutable: MutableLiveData<String> = MutableLiveData()
@@ -19,22 +19,25 @@ class MapViewModel : ViewModel() {
     private val coordinatesMutable: MutableLiveData<String> = MutableLiveData()
     val coordinates: LiveData<String> = coordinatesMutable
     private val selectionMetadataMutable: MutableLiveData<GeoObjectSelectionMetadata> = MutableLiveData()
-    val selectionMetadata: LiveData<GeoObjectSelectionMetadata> get() = selectionMetadataMutable
-    private val pointAddressConverter = PointAddressConverter(
-        object : Session.SearchListener {
-            override fun onSearchResponse(p0: Response) {
-                p0.collection.children.firstOrNull()?.let { it ->
-                    fetchAddress(it.obj!!)?.let { updateAddress(it) }
-                    updateCoordinates(it.obj!!.geometry[0].point!!)
+    val selectionMetadata: LiveData<GeoObjectSelectionMetadata>
+        get() = selectionMetadataMutable
+    private val pointAddressConverter = PointAddressConverter(searchType = SearchType.GEO.value)
+        .apply {
+            addOnAddressFetchedListener(
+                object : OnAddressFetchedListener {
+                    override fun onSuccess(address: String?) {
+                        address?.let { updateAddress(it) }
+                    }
                 }
-            }
-
-            override fun onSearchError(p0: Error) {
-                Log.e("onSearchError", p0.toString())
-            }
-        },
-        searchType = SearchType.GEO.value
-    )
+            )
+            addonGeoObjectFetchedListener(
+                object : OnGeoObjectFetchedListener {
+                    override fun onSuccess(obj: GeoObject?) {
+                        obj?.geometry?.get(0)?.point?.let { updateCoordinates(it) }
+                    }
+                }
+            )
+        }
 
     fun processSelectedObject(p0: GeoObjectTapEvent) {
         val selectionMetadata = p0
@@ -54,22 +57,17 @@ class MapViewModel : ViewModel() {
     }
 
     fun processResultItem(item: SearchResultItem) {
-        fetchAddress(item.geoObject)?.let { updateAddress(it) }
+        PointAddressConverter.fetchAddress(item.geoObject)?.let { updateAddress(it) }
         updateCoordinates(item.point)
     }
 
-    private fun fetchAddress(item: GeoObject): String? =
-        item.metadataContainer
-            .getItem(ToponymObjectMetadata::class.java)
-            ?.address?.formattedAddress
-            ?: let {
-            item.metadataContainer
-                .getItem(
-                    BusinessObjectMetadata::class.java
-                )?.address?.formattedAddress
-        }
-
     fun addressFromPoint(p: Point, zoom: Int) {
         pointAddressConverter.addressFromPoint(p, zoom)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        pointAddressConverter.removeGeoObjectFetchedListener()
+        pointAddressConverter.removeOnAddressFetchedListener()
     }
 }
