39be936dff0eda34cf743938b9707f0d7c33ce79
Ktoxsyrov <ktoxsyrovv@inbox.ru>
2021-12-03 01:46:17 +0300

    comment

---
+++ app/src/main/java/com/example/complamap/model/Comment.kt
@@ -0,0 +1,12 @@
+package com.example.complamap.model
+
+import android.media.Image
+
+data class Comment (
+    val author: String? = null,
+    val rating: String? = null,
+    val comment_text: String? = null,
+    val date: String? = null,
+    val author_profile_pic: String? = null
+
+    )
