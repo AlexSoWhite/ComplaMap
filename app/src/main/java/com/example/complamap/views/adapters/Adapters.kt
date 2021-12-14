49ecd20229ab510ca6eb2a518083521470c4feda
AlexSoWhite <shura_belov_00@bk.ru>
2021-12-14 21:17:50 +0300

    adapter

--- app/src/main/java/com/example/complamap/views/adapters/Adapters.kt
+++ app/src/main/java/com/example/complamap/views/adapters/Adapters.kt
@@ -6,19 +6,17 @@ import androidx.core.text.bold
 import androidx.core.text.underline
 import androidx.databinding.BindingAdapter
 
-class Adapters {
-    companion object {
-        @BindingAdapter(value = ["dynamicText", "specialText"], requireAll = true)
-        @JvmStatic
-        fun bindText(textView: TextView, dynamicText: String?, specialText: String?) {
-            textView.text = makePartBoldStr(specialText, dynamicText)
-        }
+object Adapters {
+    @BindingAdapter(value = ["dynamicText", "specialText"], requireAll = true)
+    @JvmStatic
+    fun bindText(textView: TextView, dynamicText: String?, specialText: String?) {
+        textView.text = makePartBoldStr(specialText, dynamicText)
+    }
 
-        private fun makePartBoldStr(beginning: String?, str: String?): SpannableStringBuilder {
-            return SpannableStringBuilder()
-                .bold { underline { append(beginning) } }
-                .bold { append(":") }
-                .append(" ${str ?: "Не задано"}")
-        }
+    private fun makePartBoldStr(beginning: String?, str: String?): SpannableStringBuilder {
+        return SpannableStringBuilder()
+            .bold { underline { append(beginning) } }
+            .bold { append(":") }
+            .append(" ${str ?: "Не задано"}")
     }
 }
